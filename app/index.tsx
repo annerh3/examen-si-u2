26f869a52aa71a88e9142155d0097413e8daaa94
annerh3 <annerh3@gmail.com>
2025-03-13 15:47:30 -0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../global.css";
+
+const index = () => {
+  return (
+    <View>
+      <Text className='font-bold text-2xl' >Alóó</Text>
+    </View>
+  )
+}
+
+export default index
